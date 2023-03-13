929ac8b02f768e7731ccf1f273ee8d6ab18eca05
Bill Hayes <bhayes84@yahoo.com>
2023-03-13 10:51:17 -0700

    Test pi function. 13mar23

--- hello.py
+++ hello.py
@@ -1,3 +1,13 @@
 def hello(name):
-    print('Howdy', name)
+    return 'Howdy' + name
+
+def pi():
+    return 3.14
+
+if __name__ == '__main__':
+    assert pi() == 3.14d
+    
+    print('All tests passed')
+    
+    
     
