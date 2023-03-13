c0a806afdb56cbd2d5d508fb74a6c00edf56a1b7
Bill Hayes <bhayes84@yahoo.com>
2023-03-13 13:19:49 -0700

    add failing test

--- hello.py
+++ hello.py
@@ -1,11 +1,17 @@
 def hello(name):
     return 'Howdy' + name
+
 def pi():
     return 3.14
 
 if __name__ == '__main__':
+    print('run pi test')
     assert pi() == 3.14
+    print('test pi PASSED')
     
+    print('run pi test that will fail')
+    assert pi() = 99
+
     print('All tests passed')
     
     
